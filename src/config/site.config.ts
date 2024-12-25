96be84bfc1de7a11093867380ca5bc8c6a139a47
rudrodip <official.rudrodipsarker@gmail.com>
2024-12-25 15:52:06 +0600

    removed blog word

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -2,7 +2,7 @@ import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
   name: "MDX Template",
-  title: "Modern MDX Blog Template with Next.js 15",
+  title: "Modern MDX Template with Next.js 15",
   description: "A sleek and modern template for building documentation and blogs with Next.js 15, MDX, Velite, and Shadcn UI components",
   origin: "https://mdx.rdsx.dev",
   keywords: [
